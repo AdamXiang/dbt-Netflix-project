b05c3818de6755861c187964711404122a683b82
adam <adam.chang@datarget.com>
2026-02-26 10:34:42 +0800

    build: mart models

---
+++ netflix/models/mart/mart_movies_releases.sql
@@ -0,0 +1,18 @@
+{{ config(materialized = 'table') }}
+
+WITH fct_ratings AS (
+    SELECT * FROM {{ ref('fct_ratings') }}
+),
+seed_dates AS (
+    SELECT * FROM {{ ref('seed_movie_release_dates') }}
+)
+SELECT
+    f.*,
+    CASE
+        WHEN s.release_date IS NULL THEN 'UNKNOWN'
+        ELSE 'KNOWN'
+    END AS release_info_available
+FROM
+    fct_ratings f
+LEFT JOIN seed_dates s
+ON f.movie_id = s.movie_id
